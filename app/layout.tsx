b372459fb26069e655cf7d18c8281ce34584616d
OmarMWarraich <earthling986@gmail.com>
2024-11-05 23:57:34 +0500

    feat: Auth Routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -40,7 +39,6 @@ export default function RootLayout({
           enableSystem={true}
           disableTransitionOnChange={true}
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
